077930528c13ca62e326e3f4989073c6bb678845
RettBehrens <everettbehrens@gmail.com>
2017-09-12 17:05:13 -0600

    right back where I started from, wasted a day, sweet

--- brews-cruise-angular/src/app/app.module.ts
+++ brews-cruise-angular/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'cli-universal-demo'})
   ],
   providers: [],
   bootstrap: [AppComponent]
