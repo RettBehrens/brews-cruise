228a826fd2e711302686d5b7780c5a84dba4abb6
RettBehrens <everettbehrens@gmail.com>
2017-09-18 11:07:25 -0600

    hopefully fixes previous commit

--- brews-cruise-angular/src/models/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, Sequelize) {
-  var model = sequelize.define("user", {
-    email: Sequelize.STRING,
-    password: Sequelize.STRING
-  });
-
-  return model;
-};
--- brews-cruise-v3-angular/src/app/apikey.service.ts
+++
@@ -1,6 +0,0 @@
-export class ApiKeyService {
-    
-    firebaseApiKey = "AIzaSyDgqxZsaq-KeYKfXSr-j-_kkxW3mEzCXI8";
-    firebaseAuthDomain = "brewscruise-722ac.firebaseapp.com";
-    
-}
